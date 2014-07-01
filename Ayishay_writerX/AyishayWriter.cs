2ddbe22f42f62deedd010a934d8df8c81ec54931
shayi <shayizhang@gmail.com>
2014-07-01 10:28:48 -0400

    Ayishay_ExcelWriter Ayishay Excel Class

---
+++ Ayishay_writerX/AyishayWriter.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Ayishay_writerX
+{
+    /* How to use this class  (this class works well on multi-sheets)
+    * static void Main()
+    * {
+    * AyishayWriter writer = new AyishayWriter(fileName);
+    * writer.DatatableToExcel(table1, sheetName1);
+    * writer.DatatableToExcel(table2, sheetName2);
+    * writer.saveExcel();
+    * Process.Start(file.Name);
+    * }
+    */
+    public class AyishayWriter
+    {
+        string filename { get; set; }
+        Excel.Application xlApp { get; set; }
+        Excel.Workbook wb { get; set; }
+
+
+        public AyishayWriter(string _filename)
+        {
+            filename = _filename;
+            xlApp = new Excel.Application(filename);
+            wb = xlApp.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+        }
+
+        public void DatatableToExcel(DataTable _table, string _sheetName)
+        {
+            wb.Sheets.Add();
+            Excel.Worksheet ws = (Excel.Worksheet)wb.Sheets[1];
+            ws.Name = _sheetName;
+
+            for (int i = 0; i < _table.Columns.Count; i++)
+            {
+                string rowIndex = "1";
+                string columnIndex = NumberToLetter(i + 1);
+                string header = _table.Columns[i].Caption;
+
+                Excel.Range aRange = ws.get_Range(columnIndex + rowIndex, columnIndex + rowIndex);
+                object[] args = new object[1];
+                args[0] = header;
+                aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+            }
+
+            for (int j = 0; j < _table.Rows.Count; j++)
+            {
+                {
+                    for (int i = 0; i < _table.Columns.Count; i++)
+                    {
+                        string rowIndex = (j + 2).ToString();
+                        string columnIndex = NumberToLetter(i + 1);
+                        Excel.Range aRange = ws.get_Range(columnIndex + rowIndex);
+                        object[] args = new object[1];
+                        args[0] = _table.Rows[j][i];
+                        aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+                    }
+                }
+            }
+
+        }
+
+        public void saveExcel()
+        {
+            wb.SaveAs(filename);
+            wb.Close();
+        }
+
+        private string NumberToLetter(int _num)
+        {
+            switch (_num)
+            {
+                case 1:
+                    return "A";
+
+                case 2:
+                    return "B";
+
+                case 3:
+                    return "C";
+
+                case 4:
+                    return "D";
+
+                case 5:
+                    return "E";
+
+
+                case 6:
+                    return "F";
+
+                case 7:
+                    return "G";
+
+                case 8:
+                    return "H";
+
+                case 9:
+                    return "I";
+
+                case 10:
+                    return "J";
+
+
+                case 11:
+                    return "K";
+
+                case 12:
+                    return "L";
+
+                case 13:
+                    return "M";
+
+                case 14:
+                    return "N";
+
+                case 15:
+                    return "O";
+
+
+
+                case 16:
+                    return "P";
+
+                case 17:
+                    return "Q";
+
+                case 18:
+                    return "R";
+
+                case 19:
+                    return "S";
+
+                case 20:
+                    return "T";
+
+
+
+                case 21:
+                    return "U";
+
+                case 22:
+                    return "V";
+
+                case 23:
+                    return "W";
+
+                case 24:
+                    return "X";
+
+                case 25:
+                    return "Y";
+
+
+
+                case 26:
+                    return "Z";
+
+            }
+            return "A";
+
+
+        }
+    }
+}
---
+++ Ayishay_writerX/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Ayishay_writerX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private DataTable TableOneGenerator()
+        {
+            DataTable table1 = new DataTable();
+            table1.Columns.Add("Column1");
+            table1.Columns.Add("Column2");
+            table1.Columns.Add("Column3");
+
+            table1.Rows.Add(new object[] { "Value1", "Value2", "Value3" });
+            table1.Rows.Add(new object[] { "Value21", "Value22", "Value23" });
+            table1.Rows.Add(new object[] { "Value31", "Value32", "Value33" });
+
+            return table1;
+        }
+    }
+}
