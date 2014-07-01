fb4e7fd3d7206357e7a6db3f66192876cf0f97fc
shayi <shayizhang@gmail.com>
2014-07-01 11:07:09 -0400

    Ayishay ExcelWriter * Save C# datatable to an Excel sheet * Can save to multiple sheets on one Excel file * can save as both .xls or .xlsx file

--- Ayishay_writerX/Program.cs
+++ Ayishay_writerX/Program.cs
@@ -13,20 +13,29 @@ namespace Ayishay_writerX
     {
         static void Main(string[] args)
         {
-        }
 
-        private DataTable TableOneGenerator()
-        {
             DataTable table1 = new DataTable();
             table1.Columns.Add("Column1");
             table1.Columns.Add("Column2");
             table1.Columns.Add("Column3");
-
             table1.Rows.Add(new object[] { "Value1", "Value2", "Value3" });
             table1.Rows.Add(new object[] { "Value21", "Value22", "Value23" });
             table1.Rows.Add(new object[] { "Value31", "Value32", "Value33" });
 
-            return table1;
+            DataTable table2 = new DataTable();
+            table2.Columns.Add("Column1");
+            table2.Columns.Add("Column2");
+            table2.Columns.Add("Column3");
+            table2.Rows.Add(new object[] { "Value221", "Value222", "Value223" });
+            table2.Rows.Add(new object[] { "Value2221", "Value2222", "Value2223" });
+            table2.Rows.Add(new object[] { "Value2231", "Value2232", "Value2233" });
+
+            string filename = "thisfile.xls";
+            AyishayWriter writer = new AyishayWriter(filename);
+            writer.DatatableToExcel(table1, "table1");
+            writer.DatatableToExcel(table2, "table2");
+            writer.saveExcel();
+            Console.ReadKey();
         }
     }
 }
